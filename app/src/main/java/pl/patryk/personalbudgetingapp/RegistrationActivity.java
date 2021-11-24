cdef777eeeaa396e90494ac8e9dd2fa67cbe53a1
Patryk Osak <pionchaoo@gmail.com>
2021-11-25 00:29:50 +0100

    add register activity

---
+++ app/src/main/java/pl/patryk/personalbudgetingapp/RegistrationActivity.java
@@ -0,0 +1,18 @@
+package pl.patryk.personalbudgetingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
