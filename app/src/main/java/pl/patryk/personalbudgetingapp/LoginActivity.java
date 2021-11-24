3f0ea19f7886b8509c0ffbb564b33bdc3910688f
Patryk Osak <pionchaoo@gmail.com>
2021-11-25 00:33:17 +0100

    add intent to register activity from login

--- app/src/main/java/pl/patryk/personalbudgetingapp/LoginActivity.java
+++ app/src/main/java/pl/patryk/personalbudgetingapp/LoginActivity.java
@@ -2,13 +2,36 @@ package pl.patryk.personalbudgetingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button loginBtn;
+    private TextView loginQn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginBtn);
+        loginQn = findViewById(R.id.loginQn);
+
+        loginQn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
