84f980bfa15fd28c51abdc3d23a166aadd231258
Patryk Osak <pionchaoo@gmail.com>
2021-11-28 23:15:08 +0100

    add intent to budget from main activity

--- app/src/main/java/pl/patryk/personalbudgetingapp/MainActivity.java
+++ app/src/main/java/pl/patryk/personalbudgetingapp/MainActivity.java
@@ -1,14 +1,29 @@
 package pl.patryk.personalbudgetingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CardView budgetCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        budgetCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BudgetActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
