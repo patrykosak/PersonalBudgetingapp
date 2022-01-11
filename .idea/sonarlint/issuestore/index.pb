8970c0a02179ff1dbd8fb082ea7302d31b9bfac5
Patryk Osak <pionchaoo@gmail.com>
2022-01-11 22:54:58 +0100

    fetching exspenses from firebase, add handle of delete and update expense

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,5 +5,3 @@ H
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
 s
 Capp/src/main/java/pl/patryk/personalbudgetingapp/LoginActivity.java,3\5\35385b817a5a1adac632f7d92d7b6fa9abb0c0de
-P
- app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
--- app/src/main/java/pl/patryk/personalbudgetingapp/TodaySpendingActivity.java
+++ app/src/main/java/pl/patryk/personalbudgetingapp/TodaySpendingActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
@@ -22,8 +23,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import org.joda.time.DateTime;
 import org.joda.time.Months;
@@ -31,7 +36,10 @@ import org.joda.time.MutableDateTime;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
 
 public class TodaySpendingActivity extends AppCompatActivity {
 
@@ -46,6 +54,9 @@ public class TodaySpendingActivity extends AppCompatActivity {
     private String onlineUserId = "";
     private DatabaseReference expensesRef;
 
+    private TodayItemsAdapter todayItemsAdapter;
+    private List<Data> myDataList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,13 +68,28 @@ public class TodaySpendingActivity extends AppCompatActivity {
 
         totalAmountSpentOn = findViewById(R.id.totalAmountSpentOn);
         progressBar = findViewById(R.id.progressBar);
-        recyclerView = findViewById(R.id.recyclerView);
+
+
         fab = findViewById(R.id.fab);
         loader = new ProgressDialog(this);
 
         mAuth = FirebaseAuth.getInstance();
         onlineUserId = mAuth.getCurrentUser().getUid();
         expensesRef = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserId);
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        linearLayoutManager.setReverseLayout(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        myDataList = new ArrayList<>();
+        todayItemsAdapter = new TodayItemsAdapter(TodaySpendingActivity.this, myDataList);
+        recyclerView.setAdapter(todayItemsAdapter);
+
+        readItems();
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,6 +98,46 @@ public class TodaySpendingActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    private void readItems() {
+
+
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar cal = Calendar.getInstance();
+        String date = dateFormat.format(cal.getTime());
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserId);
+        Query query = reference.orderByChild("date").equalTo(date);
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                myDataList.clear();
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    Data data = dataSnapshot.getValue(Data.class);
+                    myDataList.add(data);
+                }
+
+                todayItemsAdapter.notifyDataSetChanged();
+                progressBar.setVisibility(View.GONE);
+
+                int totalAmount = 0;
+                for(DataSnapshot ds: snapshot.getChildren()){
+                    Map<String, Object> map = (Map<String, Object>)ds.getValue();
+                    Object total = map.get("amount");
+                    int pTotal = Integer.parseInt(String.valueOf(total));
+                    totalAmount += pTotal;
+
+                    totalAmountSpentOn.setText("Total Day's spending: $"+totalAmount);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
@@ -128,7 +194,7 @@ public class TodaySpendingActivity extends AppCompatActivity {
                     DateTime now = new DateTime();
                     Months months = Months.monthsBetween(epoch,now);
 
-                    Data data = new Data(item, date, id, null, Integer.parseInt(Amount), months.getMonths());
+                    Data data = new Data(item, date, id, notes, Integer.parseInt(Amount), months.getMonths());
                     expensesRef.child(id).setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
