5c1d2b2477e0ae43eb675c5bd2e0828149bef4ad
Patryk Osak <pionchaoo@gmail.com>
2022-01-12 18:58:35 +0100

    intent to daily analytic

--- app/src/main/java/pl/patryk/personalbudgetingapp/ChooseAnalyticActivity.java
+++ app/src/main/java/pl/patryk/personalbudgetingapp/ChooseAnalyticActivity.java
@@ -1,14 +1,29 @@
 package pl.patryk.personalbudgetingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChooseAnalyticActivity extends AppCompatActivity {
+    private CardView todayCardView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_analytic);
+
+        todayCardView = findViewById(R.id.todayCardView);
+
+        todayCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseAnalyticActivity.this, DailyAnalyticsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
