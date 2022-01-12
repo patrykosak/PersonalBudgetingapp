b6ec31ffe06c6286356b8304cb5c4ec6afd065cc
Patryk Osak <pionchaoo@gmail.com>
2022-01-12 19:16:02 +0100

    add monthly and weekly analytics layouts

---
+++ app/src/main/java/pl/patryk/personalbudgetingapp/MonthlyAnalyticsActivity.java
@@ -0,0 +1,14 @@
+package pl.patryk.personalbudgetingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MonthlyAnalyticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monthly_analytics);
+    }
+}
