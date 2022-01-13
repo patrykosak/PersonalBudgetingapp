cccd1414ea423ed90621df688527354e7c9fd6d6
Patryk Osak <pionchaoo@gmail.com>
2022-01-13 15:22:28 +0100

    add toolbar to video

--- app/src/main/java/pl/patryk/personalbudgetingapp/VideoActivity.java
+++ app/src/main/java/pl/patryk/personalbudgetingapp/VideoActivity.java
@@ -2,6 +2,7 @@ package pl.patryk.personalbudgetingapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -12,10 +13,20 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTube
 
 public class VideoActivity extends AppCompatActivity {
     private YouTubePlayerView youTubePlayerView;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        toolbar = findViewById(R.id.video_Toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("How to save money");
+
+
 
         youTubePlayerView = findViewById(R.id.youtube_player_view);
         getLifecycle().addObserver(youTubePlayerView);
